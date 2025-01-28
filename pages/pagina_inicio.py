9a87e225cde3f8aad332fb1feffd84945fb9fa6b
Julianadevjunior <jucarlos.jv@gmail.com>
2025-01-28 12:10:37 -0300

    Update pagina_inicio.py

--- pages/pagina_inicio.py
+++ pages/pagina_inicio.py
@@ -54,10 +54,13 @@ for num_page in range(0, len(os.listdir('pages'))):
             cont = st.container(border=True, height=620)
             with cont:
                 pasta_images = [os.listdir(f'images/imovel_{num_page}')]
-                imagem = f'images/imovel_{num_page}/{pasta_images[0][0]}'
-                st.write(f'0{imagem[imagem.index('.'):]}' in pasta_images[0])
-                print('************************')
-                print(f'0{imagem[imagem.index('.'):]}' in pasta_images[0])
+                try:
+                    caminho = f"0{pasta_images[0][0][pasta_images[0][0].index('.'):]}"
+                    imagem = f'images/imovel_{num_page}/{caminho}'
+                    st.write('sim')
+                except:
+                    imagem = f'images/imovel_{num_page}/{pasta_images[0][0]}'
+                    st.write('não')
                 # Carregar e rotacionar imagem com cache
                 img = load_and_rotate_image(imagem, st.session_state["rotations"][0])
                 corrected_image = correct_image_orientation(img)
