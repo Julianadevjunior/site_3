7f08d25adfe7e44d78793135554ab1afe3fe34a4
Julianadevjunior <jucarlos.jv@gmail.com>
2025-01-28 12:49:59 -0300

    Update pagina_inicio.py

--- pages/pagina_inicio.py
+++ pages/pagina_inicio.py
@@ -59,7 +59,7 @@ for num_page in range(0, len(os.listdir('pages'))):
                 # Carregar e rotacionar imagem com cache
                 st.write(imagem[imagem.index('.'):], num_page)
 
-                st.image(f'images/imovel_{num_page}/0 foto{imagem[imagem.index('.'):]}')
+                st.write(f'images/imovel_{num_page}/0 foto{imagem[imagem.index('.'):]}')
                 img = load_and_rotate_image(imagem, st.session_state["rotations"][0])
                 corrected_image = correct_image_orientation(img)
                 dimencionado = resize_image(corrected_image, size=(300, 300))
